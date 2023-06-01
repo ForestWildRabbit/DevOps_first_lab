90cbbfd21ae86362352d39cabbdc8c62cc4399ff
rabbit <krabovedok@mail.ru>
2023-06-01 20:57:27 +0300

    Upload main file

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import sys
 
 
 def main():
-    running = True
     application_path = ""
 
     if getattr(sys, 'frozen', False):
@@ -12,20 +11,20 @@ def main():
     elif __file__:
         application_path = os.path.dirname(__file__)
 
-    while running:
-        link = input("Enter YOUTUBE link with a video: ")
-        try:
-            youtube = YouTube(link)
-            title = youtube.title.replace("/", "")
 
-            output_path = os.path.join(application_path, "audiofiles")
-            if not os.path.exists(output_path):
-                os.mkdir(output_path)
+    link = input("Enter YOUTUBE link with a video: ")
+    try:
+        youtube = YouTube(link)
+        title = youtube.title.replace("/", "")
 
-            youtube.streams.get_audio_only().download(output_path=output_path, filename=f"{title}.mp3")
-            print(f"Successfully loaded to {output_path}/{title}.mp3")
-        except exceptions.RegexMatchError:
-            print("You entered an invalid url.")
+        output_path = os.path.join(application_path, "audiofiles")
+        if not os.path.exists(output_path):
+            os.mkdir(output_path)
+
+        youtube.streams.get_audio_only().download(output_path=output_path, filename=f"{title}.mp3")
+        print(f"Successfully loaded to {output_path}/{title}.mp3")
+    except exceptions.RegexMatchError:
+        print("You entered an invalid url.")
 
     return 0
 
